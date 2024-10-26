0137122de5facb75f3df0dad7072d1c8fcff44d9
Sam Wu <swu@nait.ca>
2024-10-25 18:25:09 -0600

    Added support for multi-tenant data and RBAC control.

--- dmit2015-secure-faces-start/src/main/java/dmit2015/service/MovieService.java
+++ dmit2015-secure-faces-start/src/main/java/dmit2015/service/MovieService.java
@@ -5,6 +5,7 @@ import dmit2015.repository.MovieRepository;
 import jakarta.data.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.security.enterprise.SecurityContext;
 import jakarta.transaction.Transactional;
 
 import java.io.Serializable;
@@ -16,9 +17,16 @@ import java.util.Optional;
 public class MovieService implements Serializable {
 
     @Inject
+    private SecurityContext _securityContext;
+
+    @Inject
     private MovieRepository _movieRepository;
 
     public void addMovie(Movie newMovie) {
+        // Assign the username of this entity to the current authenticated username
+        String username = _securityContext.getCallerPrincipal().getName();
+        newMovie.setUsername(username);
+
         newMovie.setCreateTime(LocalDateTime.now());
         _movieRepository.add(newMovie);
     }
@@ -46,8 +54,16 @@ public class MovieService implements Serializable {
     }
 
     public List<Movie> findAllMovies(Sort<Movie> sort) {
-        return _movieRepository.findAll(sort);
-
+//        return _movieRepository.findAll(sort);
+        if (_securityContext.getCallerPrincipal().getName().equalsIgnoreCase("anonymous") ) {
+            throw new RuntimeException("Access Denied. Anonymous users do not have permission to access this method.");
+        }
+        if (_securityContext.isCallerInRole("Executive") ||
+                _securityContext.isCallerInRole("Administration") ) {
+            return _movieRepository.findAll(Sort.asc("title"));
+        }
+        String username = _securityContext.getCallerPrincipal().getName();
+        return _movieRepository.findAllBy(username, Sort.desc("releaseDate"));
     }
 
     public long countMovies() {
